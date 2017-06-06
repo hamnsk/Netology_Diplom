689ba383cd5b79eb17c4cf71ff9be2fea1c1a388
s.andryunin <s.andryunin@mac359rds.rambler.ramblermedia.com>
2017-06-06 14:51:24 +0300

    more fixes, fast code, style fixes

--- parser.py
+++ parser.py
@@ -17,64 +17,56 @@ class NativeVk(object):
 
     def call_api(self, method_name, args):
         url = self.vk_url.format(method=method_name)
-        result = requests.post(url, args)
-        return result.json()['response']
+        while True:
+            try:
+                result = requests.post(url, args)
+                result = result.json()['response']
+            except KeyError:
+                pass
+            else:
+                break
+        return result
 
     def fetch_groups(self, user_id):
-        args = dict()
-        args['access_token'] = self.vk_token
-        args['user_id'] = user_id
-        try:
-            result = self.call_api('groups.get', args)
-        except KeyError:
-            time.sleep(3)
-            result = self.call_api('groups.get', args)
+        args = {
+            'access_token': self.vk_token,
+            'user_id':  user_id
+        }
+        result = self.call_api('groups.get', args)
         return result
 
     def fetch_friends(self, user_id):
-        args = dict()
-        args['access_token'] = self.vk_token
-        args['user_id'] = user_id
-        try:
-            result = self.call_api('friends.get', args)
-        except KeyError:
-            time.sleep(3)
-            result = self.call_api('friends.get', args)
+        args = {
+            'access_token': self.vk_token,
+            'user_id': user_id
+        }
+        result = self.call_api('friends.get', args)
         return result
 
     def is_member(self, group_id, user_ids):
-        args = dict()
-        args['group_id'] = group_id
-        args['user_ids'] = ','.join(str(uid) for uid in user_ids)
-        args['access_token'] = self.vk_token
-        try:
-            result = self.call_api('groups.isMember', args)
-        except KeyError:
-            time.sleep(3)
-            result = self.call_api('groups.isMember', args)
+        args = {
+            'group_id': group_id,
+            'user_ids': ','.join(str(uid) for uid in user_ids),
+            'access_token': self.vk_token
+        }
+        result = self.call_api('groups.isMember', args)
         return result
 
     def fetch_by_id(self, group_ids, fields):
-        args = dict()
-        args['group_ids'] = ','.join(str(gid) for gid in group_ids)
-        args['fields'] = fields
-        args['access_token'] = self.vk_token
-        try:
-            result = self.call_api('groups.getById', args)
-        except KeyError:
-            time.sleep(3)
-            result = self.call_api('groups.getById', args)
+        args = {
+            'group_ids': ','.join(str(gid) for gid in group_ids),
+            'fields': fields,
+            'access_token': self.vk_token
+        }
+        result = self.call_api('groups.getById', args)
         return result
 
     def fetch_user_id(self, user_name):
-        args = dict()
-        args['user_ids'] = user_name
-        args['access_token'] = self.vk_token
-        try:
-            result = self.call_api('users.get', args)
-        except KeyError:
-            time.sleep(3)
-            result = self.call_api('users.get', args)
+        args = {
+            'user_ids': user_name,
+            'access_token': self.vk_token
+        }
+        result = self.call_api('users.get', args)
         result = result[0]['uid']
         return result
 
@@ -102,7 +94,7 @@ def check_member(vk_api, user_ids, groups):
 def check_member_native(vk_api, user_ids, groups):
     bad_groups = []
     for group in groups:
-        time.sleep(1)
+        # time.sleep(0.5)
         result = vk_api.is_member(group_id=group, user_ids=user_ids)
         for item in result:
             if item['member']:
@@ -118,10 +110,11 @@ def dump_to_json(response):
     with open('groups.json', 'w', encoding='utf-8') as json_file:
         data_list = []
         for item in response:
-            group = dict()
-            group['gid'] = item['gid']
-            group['members_count'] = item['members_count']
-            group['name'] = item['name']
+            group = {
+                'gid': item['gid'],
+                'members_count': item['members_count'],
+                'name': item['name']
+            }
             data_list.append(group)
 
         data = json.dumps(data_list, indent=2, sort_keys=True, separators=(',', ':'), ensure_ascii=False)
@@ -158,12 +151,6 @@ def old_method():
     step = 500
     for i in range(0, len(friends_native), step):
         groups_native = check_member_native(native_vk, friends_native[i: i + step], groups_native)
-    # while True:
-    #     if not friends_native:
-    #         break
-    #     groups_native = check_member_native(native_vk, friends_native[:500], groups_native)
-    #     friends_native = friends_native[500:]
-
     response = native_vk.fetch_by_id(group_ids=groups_native, fields='description,'
                                                                      'members_count')
     pprint(response)
